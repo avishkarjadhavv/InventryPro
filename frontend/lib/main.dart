51fc77cbc16f26a376ceba82a8da4a9452a7384e
avishkarjadhavv <omkaravishkar@gmail.com>
2026-07-24 17:11:27 +0530

    Initial Flutter Frontend Setup

---
+++ frontend/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'InventoryPro',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('InventoryPro'),
+        ),
+        body: const Center(
+          child: Text(
+            'Welcome to InventoryPro!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
